7505df28214e2bd2855ae87b668c140e3b588057
ryanchiang <ryanschiang@gmail.com>
2025-11-09 20:05:57 -0800

    add v2 widgets to dev env

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,20 +1,23 @@
-import { createRoot } from "react-dom/client";
-import { ReactGoogleReviews } from "../components";
+import { createRoot } from 'react-dom/client';
+import { ReactGoogleReviews } from '../components';
 
-const container = document.getElementById("root")!;
+const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
     <div>
         <h1>Component Development Environment</h1>
 
-        <h2>Carousel:</h2>
-        <ReactGoogleReviews
-            featurableId="example"
-            layout="carousel"
-        />
+        <h2>Carousel v1:</h2>
+        <ReactGoogleReviews featurableId="example" layout="carousel" widgetVersion="v1" />
 
-        <h2>Badge:</h2>
-        <ReactGoogleReviews featurableId="example" layout="badge" />
+        <h2>Badge v1:</h2>
+        <ReactGoogleReviews featurableId="example" layout="badge" widgetVersion="v1" />
+
+        <h2>Carousel v2:</h2>
+        <ReactGoogleReviews featurableId="example" layout="carousel" widgetVersion="v2" />
+
+        <h2>Badge v2:</h2>
+        <ReactGoogleReviews featurableId="example" layout="badge" widgetVersion="v2" />
     </div>
 );
